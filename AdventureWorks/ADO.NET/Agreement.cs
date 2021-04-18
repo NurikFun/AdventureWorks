e6102ddff17cb9f03d32f51afc709d6d59c7637d
NurikFun <nurik.iskakov.99@mail.ru>
2021-04-18 10:20:54 +0600

    Add project files.

---
+++ AdventureWorks/ADO.NET/Agreement.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdventureWorks.ADO.NET
+{
+    public class Agreement
+    {
+        private string connectionString { get; set; }
+        public Agreement()
+        {
+            connectionString = "Server=.\\SQLEXPRESS01;Database=kaspilab;Integrated Security = true;";
+        }
+
+        public void ClientAgreement(string IIN)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand()
+                    {
+                        CommandText = "[dbo].[agreementInformation]",
+                        Connection = connection,
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    cmd.Parameters.Add("IIN", SqlDbType.NVarChar).Value = IIN;
+
+                    connection.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                        Console.WriteLine($"IIN : {reader["IIN"]}, type : {reader["type"]}, " +
+                            $"IBAN : {reader["IBAN"]}, current_balance : {reader["current_balance"]}, agreement's issue date : {reader["issue_date"]}, " +
+                            $"agreement's expiration date {reader["expiration_date"]}, isActive : {reader["isActive"]}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ AdventureWorks/Program.cs
@@ -0,0 +1,29 @@
+﻿using AdventureWorks.ADO.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureWorks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agreement agreement = new Agreement();
+
+            agreement.ClientAgreement("888866664321");
+
+            using (AdventureWorksContext adventureWorks = new AdventureWorksContext())
+            {
+                foreach (Product product in adventureWorks.Product)
+                {
+                    Console.WriteLine($"Name : {product.Name}, Product Number : {product.ProductNumber}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
